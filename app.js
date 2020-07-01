9cd34a6201bf3ad628584c283e8ca16c0941caff
Ashique S <ashique047@gmail.com>
2020-07-01 17:09:49 +0530

    Project Fixed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const https = require("https");
 const bodyParser = require("body-parser");
 var exec = require('child_process').exec;
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.set('view engine','ejs')
 
 app.use(express.static("Public"));
 app.listen(3000,function(){
@@ -15,17 +15,19 @@ app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html")
 })
 
+// app.post("/",function(req,res){
+//   console.log(req.body.hashkey1);
+//   var child = exec('java -jar ./hello.jar',function(error,stdout,stderr){
+//     console.log(stdout);
+//     if(error!= null){
+//       cosole.console.log(error);
+//     }
+//     else{
+//       res.send(stdout);
+//     }
+//   });
 app.post("/",function(req,res){
   console.log(req.body.hashkey1);
-  var child = exec('java -jar ./hello.jar',function(error,stdout,stderr){
-    console.log(stdout);
-    if(error!= null){
-      cosole.console.log(error);
-    }
-    else{
-      res.send(stdout);
-    }
-  });
-  module.exports=child;
-
-})
+  const user=req.body.hashkey1;
+  res.render('page',{givenName:user});
+});
